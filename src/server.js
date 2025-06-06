57200d346e156ed7d27f528616e07d06dabe4ec2
Eone-tgx <eone1024@gmail.com>
2025-06-06 20:47:23 +0300

    hw2-mongodb

--- src/server.js
+++ src/server.js
@@ -47,7 +47,7 @@ export const setupServer = () => {
 
     res.status(200).json({
       status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
+      message: `Successfully found contact: ${contact.name}, with id ${contactId}!`,
       data: contact,
     });
   });
