3b3c56aba34d7ab9faaec4b568374a90d1940171
Eone-tgx <eone1024@gmail.com>
2025-06-12 22:03:51 +0300

    hw3-crud

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,22 @@ export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const patchContact = async (contactId, payload) => {
+  const contact = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    { new: true },
+  );
+  return contact;
+};
+
+export const deleteContactById = async (contactId) => {
+  const deletedContact = await ContactsCollection.findByIdAndDelete(contactId);
+  return deletedContact;
+};
